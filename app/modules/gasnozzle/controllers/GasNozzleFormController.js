bebd52dff3c1d3955c11c5226943314dc57048d6
Luiz Augusto <luizaugusto@sgsistemas.com.br>
2017-03-21 18:38:19 -0300

    Finalizando a primeira versão do front de bico

--- app/modules/gasnozzle/controllers/GasNozzleFormController.js
+++ app/modules/gasnozzle/controllers/GasNozzleFormController.js
@@ -1,5 +1,31 @@
 angular.module('app.gasnozzle.controllers')
-    .controller('GasNozzleFormController', [
-        function GasNozzleFormController() {
+    .controller('GasNozzleFormController', ['$state', '$scope', 'entity', 'GasNozzleService', 'GasTankService', 'GasPumpService',
+        function GasNozzleFormController($state, $scope, entity, GasNozzleService, GasTankService, GasPumpService) {
+
+            $scope.entity = angular.copy(entity.data);
+
+            $scope.searchPump = function (param) {
+                param = param || "";
+                return GasPumpService.getAdvancedSearch('obj.name like \'%' + param + '%\'').then(function (response) {
+                    return response.data.values;
+                })
+            }
+            $scope.searchTank = function (param) {
+                param = param || "";
+                return GasTankService.getAdvancedSearch('obj.name like \'%' + param + '%\'').then(function (response) {
+                    return response.data.values;
+                })
+            }
+
+            $scope.blockBtnSave = function (entity) {
+                return !entity.number || !entity.pump || !entity.tank;
+            }
+
+            $scope.update = function (entity) {
+                GasNozzleService.update(entity).then(function () {
+                    $state.go('gasnozzle.list')
+                })
+
+            }
 
         }]);
--- app/modules/gasnozzle/controllers/GasNozzleListController.js
+++ app/modules/gasnozzle/controllers/GasNozzleListController.js
@@ -1,5 +1,40 @@
 angular.module('app.gasnozzle.controllers')
-    .controller('GasNozzleListController', [
-        function GasNozzleListController() {
+    .controller('GasNozzleListController', ['$scope', 'GasNozzleService', 'gumgaController',
+        function GasNozzleListController($scope, GasNozzleService, gumgaController) {
+            gumgaController.createRestMethods($scope, GasNozzleService, 'gasnozzle');
+            $scope.gasnozzle.execute('reset');
+            $scope.gasnozzle.execute('get');
 
+            $scope.conf = {
+                columns: 'name,pump,tank,button',
+                selection: 'multi',
+                columnsConfig: [
+                    {
+                        name: 'name',
+                        size: 'col-md-7',
+                        title: '<strong gumga-translate-tag="gasnozzle.number">Name</strong>',
+                        content: '<div> {{$value.number}} </div>'
+                    }, {
+                        name: 'tank',
+                        size: 'col-md-2',
+                        title: '<strong gumga-translate-tag="gasnozzle.tankname">Name</strong>',
+                        content: '<div> {{$value.tank.name}} </div>'
+                    },{
+                        name: 'pump',
+                        size: 'col-md-2',
+                        title: '<strong gumga-translate-tag="gasnozzle.pumpname">Name</strong>',
+                        content: '<div> {{$value.pump.name}} </div>'
+                    },
+                    {
+                        name: 'button',
+                        title: ' ',
+                        size: 'col-md-1',
+                        content: '<div align="center"><button type="button" ui-sref="gasnozzle.edit({id: $value.id})" grands-button="edit-sm" uib-tooltip="Editar"></button></div>'
+                    }
+                ]
+            };
+
+            $scope.gasnozzle.on('deleteSuccess', function(){
+                $scope.gasnozzle.execute('get');
+            })
         }]);
