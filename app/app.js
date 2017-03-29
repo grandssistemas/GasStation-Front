8c0e39048f20f41f2114422620b1006511871d3e
Augusto Cesar <augusto.gonzaga2010@gmail.com>
2017-03-29 09:09:34 -0300

    alteração base.html para baseGasStation.html

--- app/app.js
+++ app/app.js
@@ -1,32 +1,31 @@
 angular.module('gas.core', [
-        'ui.router',
-        'ngSanitize',
-        'app.gasnozzle',
-        'app.gaspump',
-        'app.gastank',
-        'gumga.core',
-        'grands.components',
-        'product.core',
-        'gas.templates'
-    ]).config(['$stateProvider', function ($stateProvider) {
+    'ui.router',
+    'ngSanitize',
+    'app.gasnozzle',
+    'app.gaspump',
+    'app.gastank',
+    'gumga.core',
+    'grands.components',
+    'product.core',
+    'gas.templates'
+]).config(['$stateProvider', function ($stateProvider) {
 
-        $stateProvider
-            .state('gaspump',{
-                data: { id : 1},
-                url: '/gaspump',
-                templateUrl : 'base.html'
-            })
-            .state('gastank',{
-                data: { id : 1},
-                url: '/gastank',
-                templateUrl : 'base.html'
-            })
-            .state('gasnozzle',{
-                data: { id : 1},
-                url: '/gasnozzle',
-                templateUrl : 'base.html'
-            })
+    $stateProvider
+        .state('gaspump', {
+            data: {id: 1},
+            url: '/gaspump',
+            templateUrl: 'baseGasStation.html'
+        })
+        .state('gastank', {
+            data: {id: 1},
+            url: '/gastank',
+            templateUrl: 'baseGasStation.html'
+        })
+        .state('gasnozzle', {
+            data: {id: 1},
+            url: '/gasnozzle',
+            templateUrl: 'baseGasStation.html'
+        })
 
-
-    }])
+}]);
 
